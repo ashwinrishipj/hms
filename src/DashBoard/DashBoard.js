6187fabdf4f7f9f86b8eeedd04ae6c01ac4be123
Ashwin Rishi <ashwinrishipj@gmail.com>
2020-10-05 23:04:54 -0400

    updation on 05-10-2020

--- src/DashBoard/DashBoard.js
+++ src/DashBoard/DashBoard.js
@@ -4,13 +4,14 @@ import "./DashBoard.css";
 import {
   Nav,
   Navbar,
-  Form,
-  FormControl,
   Button,
   Container,
   Row,
   Col,
+  OverlayTrigger,
+  Popover
 } from "../../node_modules/react-bootstrap";
+import Search from "../helpers/search";
 
 const UserData = React.createContext();
 export const UserConsumer = UserData.Consumer;
@@ -21,11 +22,53 @@ class DashBoard extends React.Component {
     this.state = {
       handleNavigationMenu: this.handlemenu,
       navigationIsSet: true,
+      loadContent: "home",
+      loadUserSettings: false,
+      collapsed: false,
+      currentUser: true,
+      profileSelected: true,
+      searchedContent: "",
     };
   }
 
   handlemenu = () => {
-    this.setState({ navigationIsSet : !this.state.navigationIsSet});
+    this.setState({ navigationIsSet: !this.state.navigationIsSet });
+  };
+
+  handleSearch = (searchedContent) => {
+    this.setState({ loadContent: "home", searchedContent: searchedContent });
+  };
+
+  handleCall = (e) => {
+    e.preventDefault();
+    this.setState({
+      loadContent: e.target.name,
+    });
+  };
+
+  handleUserSettings = () => {
+    this.setState({
+      loadUserSettings: true,
+    });
+  };
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  handleLogout = () => {
+    localStorage.clear();
+  };
+
+  profileClick = (e) => {
+    e.preventDefault();
+    this.setState({ profileSelected: false, loadContent: e.target.name });
+  };
+
+  handleNotifications = () => {
+    alert("notified:");
   };
 
   render() {
@@ -35,21 +78,15 @@ class DashBoard extends React.Component {
           <Container fluid="true">
             <Row style={{ marginLeft: "0", marginRight: "0" }}>
               {this.state.navigationIsSet ? (
-                <Col id ="navbar" sm={2} className="leftView slideIn">
+                <Col id="navbar" sm={2} className="leftView slideIn">
                   <Nav defaultActiveKey="/home" className="flex-column">
-                    <Nav.Link>Active</Nav.Link>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                    <Nav.Link>Appointment Scheduler</Nav.Link>
+                    <Nav.Link eventKey="link-1">upload documents</Nav.Link>
                     <Nav.Link eventKey="link-2">Link</Nav.Link>
                   </Nav>
                 </Col>
               ) : (
-                <Col id ="navbar" sm={1} className="leftView slideOut">
-                  <Nav defaultActiveKey="/home" className="flex-column">
-                    <Nav.Link>Active</Nav.Link>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
-                    <Nav.Link eventKey="link-2">Link</Nav.Link>
-                  </Nav>
-                </Col>
+                ""
               )}
               <Col style={{ paddingLeft: "0", paddingRight: "0" }}>
                 <Navbar bg="dark" variant="dark">
@@ -57,14 +94,121 @@ class DashBoard extends React.Component {
                     <i class="fa fa-bars" aria-hidden="true"></i>
                   </Navbar.Brand>
                   <Nav className="mr-auto">
-                  <Form inline>
-                    <FormControl
-                      type="text"
-                      placeholder="Search"
-                      className="mr-sm-2"
-                    />
-                    <Button variant="outline-info">Search</Button>
-                  </Form>
+                    <div id="navbarSupportedContent">
+                      <ul className="navbar-nav mx-auto">
+                        <li className="nav-item">
+                          <a
+                            className="nav-link pointer fa fa-home"
+                            name="home"
+                            onClick={this.handleCall}
+                            href="#home"
+                          >
+                            Home
+                          </a>
+                        </li>
+                        <li
+                          className="nav-item"
+                          name="blogs"
+                          onClick={this.handleCall}
+                        >
+                          <a
+                            className="nav-link pointer fa fa-newspaper-o "
+                            name="blog"
+                            href="#blog"
+                          >
+                            Blog
+                          </a>
+                        </li>
+                        <li>
+                          <Search/>
+                        </li>
+                     <li className="float-right">
+                      <OverlayTrigger
+                        trigger="click"
+                        key="bottom"
+                        rootCloseEvent="click"
+                        onHide="true"
+                        rootClose="true"
+                        placement="bottom"
+                        ref="overlay"
+                        overlay={
+                          <Popover
+                            id={`popover-positioned-bottom`}
+                            title="username"
+                          >
+                            <Popover.Title as="h3">{`Popover bottom`}</Popover.Title>
+                            <Popover.Content>
+                              <div
+                                className="list-group"
+                                style={{ border: "none !important" }}
+                                id="list-tab"
+                                role="tablist"
+                              >
+                                <a
+                                  className="list-group-item selectItem list-group-item-action"
+                                  tabIndex="0"
+                                  href="#list-home"
+                                  name="profile"
+                                  onClick={this.profileClick}
+                                >
+                                  <span
+                                    className=" fa fa-address-book-o mr-2"
+                                    aria-hidden="true"
+                                  ></span>
+                                  profile
+                                </a>
+                                <a
+                                  className="list-group-item selectItem list-group-item-action"
+                                  tabIndex="1"
+                                  href="#list-profile"
+                                  name="messages"
+                                  onClick={this.profileClick}
+                                >
+                                  <span
+                                    className=" fa fa-envelope mr-2"
+                                    aria-hidden="true"
+                                  ></span>
+                                  messages
+                                </a>
+                                <a
+                                  className="list-group-item selectItem list-group-item-action"
+                                  tabIndex="2"
+                                  href="#list-messages"
+                                  onClick={this.profileClick}
+                                  name="settings"
+                                >
+                                  <i className="fa fa-gear mr-2"></i>
+                                  settings
+                                </a>
+                                <a
+                                  className="list-group-item selectItem list-group-item-action"
+                                  tabIndex="3"
+                                  href="#list-settings"
+                                  name="signout"
+                                  onClick={this.handleLogout}
+                                >
+                                  <i className="fa fa-sign-out mr-2"></i>
+                                  Logout
+                                </a>
+                              </div>
+                            </Popover.Content>
+                          </Popover>
+                        }
+                      >
+                        <Button variant="outline-dark pointer text-white">
+                          <span
+                            className="fa fa-user fa-lg"
+                            aria-hidden="true"
+                          ></span>
+                          <span
+                            className="fa fa-caret-down fa-xs"
+                            aria-hidden="true"
+                          ></span>
+                        </Button>
+                      </OverlayTrigger>
+                      </li>
+                      </ul>
+                    </div>
                   </Nav>
                 </Navbar>
               </Col>
