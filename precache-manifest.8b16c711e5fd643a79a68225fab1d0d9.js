81be28d067f0d6b16c01f6add6c33a00d3b4fa6a
Ashwin Rishi <ashwinrishipj@gmail.com>
2020-10-20 21:56:45 -0400

    Updates

---
+++ precache-manifest.8b16c711e5fd643a79a68225fab1d0d9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f47b7a3974cca2d9496d2aa11bdecd5",
+    "url": "/HMS/index.html"
+  },
+  {
+    "revision": "82d889088c7946956416",
+    "url": "/HMS/static/css/2.553583bc.chunk.css"
+  },
+  {
+    "revision": "13bc3bdeef29d91bea2c",
+    "url": "/HMS/static/css/main.01af9f8e.chunk.css"
+  },
+  {
+    "revision": "82d889088c7946956416",
+    "url": "/HMS/static/js/2.a7f5f2a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/HMS/static/js/2.a7f5f2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bc3bdeef29d91bea2c",
+    "url": "/HMS/static/js/main.cf32ed7d.chunk.js"
+  },
+  {
+    "revision": "840d1b0f2208de0c12c2",
+    "url": "/HMS/static/js/runtime-main.973daac0.js"
+  },
+  {
+    "revision": "b8ff7213f72484db743cf911d62c2bd8",
+    "url": "/HMS/static/media/BC.b8ff7213.png"
+  },
+  {
+    "revision": "aeecfa031f860c8090ab2760c5483c33",
+    "url": "/HMS/static/media/Blur-4.aeecfa03.jpg"
+  },
+  {
+    "revision": "267f6873f2d9c9b5b8414efd6fad7280",
+    "url": "/HMS/static/media/ENT.267f6873.png"
+  },
+  {
+    "revision": "98297da0ba583ec9b019730b892352b0",
+    "url": "/HMS/static/media/Gastroenterology.98297da0.png"
+  },
+  {
+    "revision": "f1fb17ddbfcee6086ccd6d114bfee9b0",
+    "url": "/HMS/static/media/Manitoba.f1fb17dd.jpg"
+  },
+  {
+    "revision": "61d1966155c87f61dff2c808f2cfa2cc",
+    "url": "/HMS/static/media/Onco.61d19661.jpg"
+  },
+  {
+    "revision": "655429e59c6ed0d9ec12a032fc236fc4",
+    "url": "/HMS/static/media/Profile.655429e5.png"
+  },
+  {
+    "revision": "10ddb6dabe966682e443ff2d375fe036",
+    "url": "/HMS/static/media/Quebec.10ddb6da.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/HMS/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/HMS/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/HMS/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/HMS/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/HMS/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "34e760b42feba2dda4107dc7948ee47d",
+    "url": "/HMS/static/media/neurology.34e760b4.png"
+  }
+]);
